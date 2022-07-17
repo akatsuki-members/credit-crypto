7522ff44e5567b5c7de4f5bb0cba704c27a0030f
fernando ocampo <fdocampo@gmail.com>
2022-07-17 22:34:24 +0200

    [29] fix health visibility
    * problem: health logic is inside an internal package and endpoints package is asking for health.Report and health.HealthChecker, those won't be visible for other projects, so it could not be used by other projects.
    * solution: create own structs that can be visible for othe projects.

--- libs/common-endpoints/pkg/endpoints/endpoint.go
+++ libs/common-endpoints/pkg/endpoints/endpoint.go
@@ -7,6 +7,21 @@ import (
 	"github.com/akatsuki-members/credit-crypto/libs/common-endpoints/internal/handlers/heartbeat"
 )
 
+// HealthChecker function type to check service health
+type HealthChecker func() Report
+
+// Item contains information about components and their status.
+type Item struct {
+	Name    string `json:"name"`    // integration or component name
+	Healthy bool   `json:"healthy"` // health status of the component.
+}
+
+// Contains health report
+type Report struct {
+	Healthy bool   `json:"healthy"`          // The service is healthy
+	Data    []Item `json:"report,omitempty"` // health report
+}
+
 type handler struct {
 	router       *http.ServeMux
 	hasEndpoints bool
@@ -25,8 +40,25 @@ func (h *handler) WithHeartbeat() *handler {
 	return h
 }
 
-func (h *handler) WithHealth(checker health.HealthChecker) *handler {
+func (h *handler) WithHealth(checker HealthChecker) *handler {
 	h.hasEndpoints = true
-	health.Add(h.router, checker)
+	health.Add(h.router, h.newHealthChecker(checker))
 	return h
+}
+
+func (h *handler) newHealthChecker(checker HealthChecker) health.HealthChecker {
+	return func() health.Report {
+		result := checker()
+		report := make([]health.Item, len(result.Data))
+		for idx, v := range result.Data {
+			report[idx] = health.Item{
+				Name:    v.Name,
+				Healthy: v.Healthy,
+			}
+		}
+		return health.Report{
+			Healthy: result.Healthy,
+			Data:    report,
+		}
+	}
 }
